e0f67e4e958a23bb75ce4e6c52d26abe9ae7ccfd
Alexander Rovang <wolf_vii@yahoo.com>
2021-01-21 14:33:10 -0500

    commit tree 1ac84be954e9ff4cec04ece932650953b2a3e0b8

---
+++ app/application.rb
@@ -0,0 +1,26 @@
+require 'pry'
+class Application
+
+  def call(env)
+
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item = req.path.split("/items/").last
+      @@items.find do |x|
+        if x.name == item
+          resp.write x.price
+          resp.status = 200
+        else
+          resp.write "Item not found"
+          resp.status = 400
+        end
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+end
